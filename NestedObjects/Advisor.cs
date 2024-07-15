2d905dd7eafbd20a3dcbe2a49dd8f68b6338ada4
Scott <greatvstech@gmail.com>
2024-07-15 01:23:19 -0700

    Created student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college avisor
+    /// </summary>
+    internal class Advisor
+    {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address for the advisor
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Building and room number where the advisor's office is located
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// represents a individual college student
+    /// </summary>
+    public class Student // class declaration
+    {
+        /// <summary>
+        /// The student's legal first name
+        /// </summary>
+        public string FirstName { get; set; } // auto-implemented property
+
+        /// <summary>   
+        /// The student's legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>   
+        /// The assigned school email address for the student
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
+
